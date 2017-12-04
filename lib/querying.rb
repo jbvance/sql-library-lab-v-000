c7265cae37c4a6ddefd0b9163b4cb233ba47bc5a
Jason Vance <jbvance@gmail.com>
2017-12-04 22:32:46 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,9 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series_id, species, count(species) FROM characters GROUP BY series_id, species HAVING species='human' ORDER BY count(species) DESC LIMIT 1;"
+  "SELECT series.title FROM characters
+INNER JOIN series ON series.id = characters.series_id
+GROUP BY series_id, species HAVING species='human' ORDER BY count(species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
